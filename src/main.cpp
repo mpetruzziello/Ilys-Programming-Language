c82a0dbeb9489f0f43f7e1f6af4b4d907c932e1d
Matthew <matthew.petruzziello@mail.mcgill.ca>
2024-08-07 16:11:00 -0400

    Update 0.0.1
    Creating an algebraic test-case (simple BEDMAS) and creating the main.cpp to read and convert the binary file to source code to lex/parse.

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+int main() {
+
+    // Opening the binary file
+    std::ifstream file("../Test_Cases/cases.ilys", std::ios::binary);
+    
+    if (!file.is_open()) {
+        std::cerr << "Error: cases.ilys file" << std::endl;
+        return 1;
+    }
+
+    std::vector<char> bytes((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+
+    // Converting the bytes to a string representing the source code
+    std::string sourceCode(bytes.begin(), bytes.end());
+}
