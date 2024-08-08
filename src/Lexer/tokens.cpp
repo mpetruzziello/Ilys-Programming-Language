093c37faa32247a3d042dba4f3ceff56ae5fd914
Matthew <matthew.petruzziello@mail.mcgill.ca>
2024-08-07 21:39:27 -0400

    Update 0.0.3
    Created Token class in tokens.cpp and added methods for creating, mapping, and checking tokens.

--- src/Lexer/tokens.cpp
+++ src/Lexer/tokens.cpp
@@ -4,22 +4,167 @@
 enum class TokenType {
     E0F_TOKEN,
 
-    OPENBRACKET_TOKEN,
-    CLOSEBRACKET_TOKEN,
+    OPENBRACKET
+    CLOSEBRACKET
 
-    OPENCURLYBRACKET_TOKEN,
-    CLOSECURLYBRACKET_TOKEN,
+    OPENCURLYBRACKET
+    CLOSECURLYBRACKET
 
-    OPENPARENTHESIS_TOKEN,
-    CLOSEPARENTHESIS_TOKEN,
+    OPENPARENTHESIS
+    CLOSEPARENTHESIS
 
-    NUMBER_TOKEN,
-    STRING_TOKEN,
+    DOT,
+    DOTDOT, // ex: for 0..3 loop (0,1,2,3)
 
-    IDENTIFIER_TOKEN,
+    NUMBER,
+    STRING,
 
-    ASSIGNMENT_TOKEN, // =
-    EQUALS_TOKEN, // ==
-    NOT_TOKEN, // ! (8:35)
+    IDENTIFIER,
 
-}
+    ASSIGNMENT, // =
+    EQUALS, // ==
+    NOT, // !
+    NOTEQUALS, // !=
+
+    AND,
+    OR,
+    COLON,
+    SEMICOLON,
+    COMMA,
+    QUESTIONMARK,
+    
+    LESSTHAN,
+    LESSTHANEQUALS,
+    GREATERTHAN,
+    GREATERTHANEQUALS,
+
+    //incrementation / decrementation
+    PLUSPLUS,
+    PLUSEQUALS,
+    MINUSMINUS,
+    MINUSEQUALS,
+    MULTIPLYEQUALS,
+    DIVIDEEQUALS,
+    MODEQUALS,
+
+    //operators
+    PLUS,
+    MINUS,
+    MULTIPLY,
+    DIVIDE,
+    MODULO,
+
+    //bool
+    TRUE,
+    FALSE,
+
+    //kw
+    IF,
+    ELSE,
+    FROM,
+    FUNC, // declare functions
+    LET,
+    CONST,
+    TYPEOF,
+    NEW,
+    IMPORT,
+    EXPORT,
+    CLASS,
+    FOREVERY, // for each loop
+    FOR,
+    WHILE,
+    IN
+};
+
+class Token {
+    public:
+        TokenType type;
+        std::string value;
+
+        std::string GetType() const {
+            switch (type) {
+                case TokenType::E0F_TOKEN: return "EOF";
+                case TokenType::OPENBRACKET: return "OPEN BRACKET";
+                case TokenType::CLOSEBRACKET: return "CLOSE BRACKET";
+                case TokenType::OPENCURLYBRACKET: return "OPEN CURLY BRACKET";
+                case TokenType::CLOSECURLYBRACKET: return "CLOSE CURLY BRACKET";
+                case TokenType::OPENPARENTHESIS: return "OPEN PARENTHESIS";
+                case TokenType::CLOSEPARENTHESIS: return "CLOSE PARENTHESIS";
+                case TokenType::DOT: return "DOT";
+                case TokenType::DOTDOT: return "DOT DOT";
+                case TokenType::NUMBER: return "NUMBER";
+                case TokenType::STRING: return "STRING";
+                case TokenType::IDENTIFIER: return "IDENTIFIER";
+                case TokenType::ASSIGNMENT: return "ASSIGNMENT";
+                case TokenType::EQUALS: return "EQUALS";
+                case TokenType::NOT: return "NOT";
+                case TokenType::NOTEQUALS: return "NOT EQUALS";
+                case TokenType::AND: return "AND";
+                case TokenType::OR: return "OR";
+                case TokenType::COLON: return "COLON";
+                case TokenType::SEMICOLON: return "SEMICOLON";
+                case TokenType::COMMA: return "COMMA";
+                case TokenType::QUESTIONMARK: return "QUESTION MARK";
+                case TokenType::LESSTHAN: return "LESS THAN";
+                case TokenType::LESSTHANEQUALS: return "LESS THAN OR EQUAL TO";
+                case TokenType::GREATERTHAN: return "GREATER THAN";
+                case TokenType::GREATERTHANEQUALS: return "GREATER THAN OR EQUAL TO";
+                case TokenType::PLUSPLUS: return "PLUS PLUS";
+                case TokenType::PLUSEQUALS: return "PLUS EQUALS";
+                case TokenType::MINUSMINUS: return "MINUS MINUS";
+                case TokenType::MINUSEQUALS: return "MINUS EQUALS";
+                case TokenType::MULTIPLYEQUALS: return "MULTIPLY EQUALS";
+                case TokenType::DIVIDEEQUALS: return "DIVIDE EQUALS";
+                case TokenType::MODEQUALS: return "MOD EQUALS";
+                case TokenType::PLUS: return "PLUS";
+                case TokenType::MINUS: return "MINUS";
+                case TokenType::MULTIPLY: return "MULTIPLY";
+                case TokenType::DIVIDE: return "DIVIDE";
+                case TokenType::MODULO: return "MODULO";
+                case TokenType::IF: return "IF";
+                case TokenType::ELSE: return "ELSE";
+                case TokenType::FROM: return "FROM";
+                case TokenType::FUNC: return "FUNC";
+                case TokenType::LET: return "LET";
+                case TokenType::CONST: return "CONST";
+                case TokenType::TYPEOF: return "TYPEOF";
+                case TokenType::NEW: return "NEW";
+                case TokenType::IMPORT: return "IMPORT";
+                case TokenType::EXPORT: return "EXPORT";
+                case TokenType::CLASS: return "CLASS";
+                case TokenType::FOREVERY: return "FOR EACH";
+                case TokenType::FOR: return "FOR";
+                case TokenType::WHILE: return "WHILE";
+                case TokenType::IN: return "IN";
+                case TokenType::TRUE: return "TRUE";
+                case TokenType::FALSE: return "FALSE";
+            }
+        }
+
+        
+        static Token ConstructToken(TokenType type, std::string value) {
+            Token token;
+            token.type = type;
+            token.value = value;
+            return token;
+        }
+
+        std::string Debug() const {
+            // the value of the identifier, number or string is necessary because it is not always the same. ex: true is always 1 in value 
+            if (type == TokenType::IDENTIFIER || type == TokenType::NUMBER || type == TokenType::STRING) {
+                return GetType() + " : " + value;
+            }
+            else {
+                return GetType();
+            }
+        }
+
+        static bool IsKnown(std::initializer_list<TokenType> possibilities, TokenType type) {
+            for (auto possibility : possibilities) {
+                if (possibility == type) {
+                    return true;
+                }
+            }
+            return false;
+        }
+};
