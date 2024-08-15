682096a3d3e2f730d33550e8b276a7aac5d17015
Matthew <matthew.petruzziello@mail.mcgill.ca>
2024-08-15 15:04:34 -0400

    Update 0.0.4-2
    Creating Helper functions for updating the patterns field of the lexer within the ConstructLexer function --> outside of Lexer class.

--- src/Lexer/lexer.cpp
+++ src/Lexer/lexer.cpp
@@ -35,6 +35,26 @@ class Lexer {
         std::vector<RegexPattern> Patterns;
 };
 
+// defining a function called "LexAdvance" to advance the lexer's position by a given amount (int)
+void LexAdvance(Lexer* lexer, int amount) {
+    lexer->Position += amount; // changing the position field of the lexer
+}
+
+// defining a function called "TokenPush" to append a token to the lexer's tokens vector
+void TokenPush(Lexer* lexer, Token token) {
+    lexer->Tokens.push_back(token);
+}
+
+// defining a function called "defaultHandler" which takes a type TokenType and a string value and returns a RegexHandler
+RegexHandler defaultHandler(TokenType type, std::string value) {
+    return [type, value](Lexer* lexer, std::regex* regex) {
+        // advancing the lexer's position just past the value we matched and pushing a token
+        LexAdvance(lexer, value.length());
+        TokenPush(lexer, Token(type, value));
+    }
+}
+
+
 // defining a function that creates a lexer by taking a source string and returning a lexer pointer
 Lexer* ConstructLexer(std::string source) {
     Lexer* lexer = new Lexer();
